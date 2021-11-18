0094f45a570afdc60c20bd48af3c9894110baad9
Bor Bálint <br.blnt10@gmail.com>
2021-11-18 20:34:14 +0100

    create pages

--- src/main/java/hu/nye/rft/web/controller/PageController.java
+++ src/main/java/hu/nye/rft/web/controller/PageController.java
@@ -1,10 +1,33 @@
 package hu.nye.rft.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Controller that returns pages.
  */
 @Controller
 public class PageController {
+
+    private static final String USER_PAGE_LOGIN = "/login";
+    private static final String ROOT_MAPPING = "/";
+    private static final String TEACHER_SIDE = "/teacherSide";
+    private static final String STUDENT_SIDE = "/studentSide";
+    private static final String SUBJECT_SIDE = "/subject/{id}";
+
+    @RequestMapping(USER_PAGE_LOGIN)
+    public String login() {
+        return "login";
+    }
+
+
+    @RequestMapping(TEACHER_SIDE)
+    public String teacherSide() {
+        return "teacherSide";
+    }
+
+    @RequestMapping(STUDENT_SIDE)
+    public String studentSide() {
+        return "studentSide";
+    }
 }
--- src/main/java/hu/nye/rft/web/domain/Subject.java
+++ src/main/java/hu/nye/rft/web/domain/Subject.java
@@ -12,6 +12,7 @@ public class Subject {
     private String name;
     private int teacherId;
     private String date; // DDD#ST:ST#ET:ET  -> MON#11:00#12:00  Every monday from 11:00 to 12:00 24h form
+    private String classroom; // BUILDING#NUMBEROFCLASSROOM ->  A#123
     private int maxParticipants;
     private int participants;
 
